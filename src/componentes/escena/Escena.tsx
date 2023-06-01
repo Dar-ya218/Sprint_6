010946448aabe1e41b885c3348be220b1aa7ade8
Dar-ya218 <dasharoura13465@gmail.com>
2023-06-01 10:43:24 +0200

    ejercicio_1

---
+++ src/componentes/escena/Escena.tsx
@@ -0,0 +1,7 @@
+function MakeWelcome()  {
+    return (
+    <div> 
+    <h3>Nuestro héroe estaba saliendo por el espacio sideral cuando en la lejanía alivió una nave espacial</h3>
+    </div>);
+}
+export default MakeWelcome;
---
+++ src/componentes/escena/datos.tsx
@@ -0,0 +1,16 @@
+/* import React from 'react'
+
+const datos = () => {
+  return (
+    <div>
+      [
+"Nuestro héroe estaba saliendo por el espacio sideral cuando en la lejanía alivió una nave espacial",
+"Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas.,
+"El héroe decidió atravesar la puerta que lo llevaba a casa", dijo.
+"Mientras tanto, otros héroes no tuvieron tanta suerte en su elección ..."
+]
+    </div>
+  )
+}
+
+export default datos */
